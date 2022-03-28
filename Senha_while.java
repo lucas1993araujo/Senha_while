5100050b2f02ae0ce4e3a881dbbb4b53a7dac462
Lucas <lucas2010araujo@hotmail.com>
2022-03-28 18:26:55 -0300

    senha

---
+++ Senha_while.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Senha_while {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int senha = sc.nextInt();
+		
+		while(senha != 2002){
+			System.out.println("Senha Invalida");
+			senha = sc.nextInt();
+		}
+		System.out.println("Acesso Permitido");
+		
+		
+		sc.close();
+	}
+
+}
